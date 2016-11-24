1c5438e4ef2744b3cb6a2b5214d12852ff570722
RaidenRabit <abirta05@yahoo.com>
2016-11-24 13:47:49 +0100

    Entity class getter and setters

--- .src/ModelLayer/Entity.java
+++ .src/ModelLayer/Entity.java
@@ -4,4 +4,45 @@ package ModelLayer;
  * Created by RedJohn on 23-Nov-16.
  */
 public abstract class Entity {
+    private String name, address, email, city, phone;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
