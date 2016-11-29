e4a7c94018bc34037a158169a6d3aeea7df015fd
stefan <amxx@abv.bg>
2016-11-29 09:48:23 +0100

    Remove the static

--- .src/ModelLayer/EmployeeContainer.java
+++ .src/ModelLayer/EmployeeContainer.java
@@ -6,22 +6,25 @@ import java.util.ArrayList;
  * Created by RedJohn on 23-Nov-16.
  */
 public abstract class EmployeeContainer {
-    public static ArrayList<Employee> employees = new ArrayList<>();
+    private  ArrayList<Employee> employees;
 
+    public EmployeeContainer() {
+        employees = new ArrayList<>();
+    }
 
-    public static void addEmployee(Employee employee) {
+    public  void addEmployee(Employee employee) {
         employees.add(employee);
     }
 
-    public  static Employee getEmployee(int index) {
+    public  Employee getEmployee(int index) {
         return employees.get(index);
     }
 
-    public static int getEmployeesSize() {
+    public  int getEmployeesSize() {
         return employees.size();
     }
 
-    public static void removeEmployee(int index) {
+    public  void removeEmployee(int index) {
         employees.remove(index);
     }
 
